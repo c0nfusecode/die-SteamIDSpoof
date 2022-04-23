7d9f4d5d7ae2392fa608525f40f03687623b910a
c0nfusecode <55307836+c0nfusecode@users.noreply.github.com>
2022-04-23 15:47:07 +0600

    main file

---
+++ anti_steamid_spoofer.lua
@@ -0,0 +1,5 @@
+hook.Add("PlayerInitialSpawn", "FuckYourself", function(ply)
+    if util.SteamIDTo64(ply:SteamID()) ~= ply:SteamID64() then
+        ply:Kick('Говна пожри и верни разработки своему хозяину')
+    end
+end)
