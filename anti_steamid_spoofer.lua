456cfc88be0201a07bd806c2c86b577fc26bf26e
c0nfusecode <55307836+c0nfusecode@users.noreply.github.com>
2022-04-23 17:25:53 +0600

    Update anti_steamid_spoofer.lua

--- anti_steamid_spoofer.lua
+++ anti_steamid_spoofer.lua
@@ -1,3 +1,4 @@
+// server side code
 hook.Add("PlayerInitialSpawn", "FuckYourself", function(ply)
     if util.SteamIDTo64(ply:SteamID()) ~= ply:SteamID64() then
         ply:Kick('Говна пожри и верни разработки своему хозяину')
